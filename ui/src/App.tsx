3bc85bdec3347f3a1a21899349c19a19322ac708
Jannick Viets <jannick@viets.email>
2024-05-02 12:07:39 +0200

    UI

---
+++ ui/src/App.tsx
@@ -0,0 +1,15 @@
+import { Upload } from "./Components/Upload";
+
+export default function App() {
+  return (
+    <div
+      style={{
+        margin: "5%",
+        display: "flex",
+        flex: 1,
+      }}
+    >
+      <Upload />
+    </div>
+  );
+}
