38b04f5ba3794b2395c4f73cb1d7e69575814fe9
Jannick Viets <jannick@viets.email>
2024-05-13 08:19:24 +0200

    Clipboard iOS

--- ui/src/Utils/copy.ts
+++ ui/src/Utils/copy.ts
@@ -1,6 +1,22 @@
 export function copy(text: string) {
   if (navigator?.clipboard?.writeText) {
     return navigator.clipboard.writeText(text);
+  } else {
+    const textArea = document.createElement("textArea") as HTMLTextAreaElement;
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    if (navigator.userAgent.match(/ipad|iphone/i)) {
+      const range = document.createRange();
+      range.selectNodeContents(textArea);
+      const selection = window.getSelection();
+      selection?.removeAllRanges();
+      selection?.addRange(range);
+      textArea.setSelectionRange(0, 999999);
+    } else {
+      textArea.select();
+    }
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+    return Promise.resolve();
   }
-  return Promise.reject("The Clipboard API is not available.");
 }
