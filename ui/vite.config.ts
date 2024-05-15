1ea68e195ef2e887ea548162ead8381704ae11e3
henobi <hendrik@obermayer.cloud>
2024-05-15 22:19:12 +0200

    Added PWA

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "safari-pinned-tab.svg"],
+      manifest: {
+        name: "File Transfer",
+        short_name: "File Transfer",
+        description: "File Transfer App",
+        theme_color: "#f8f8f8",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
