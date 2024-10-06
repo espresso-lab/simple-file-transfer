076a9ab6806322a3b7e948b50de651afa534836c
Jannick Viets <jannick@viets.email>
2024-10-06 19:37:22 +0200

    Actix.rs framework instead of Salvo.rs & local development optimization via docker compose watch

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,44 +1,29 @@
-use aws_config::BehaviorVersion;
+use actix_cors::Cors;
+use actix_files as fs;
+use actix_web::{get, middleware::Logger, post, web, App, HttpResponse, HttpServer, Responder};
+use aws_config::{BehaviorVersion, SdkConfig};
 use aws_sdk_s3 as s3;
-use once_cell::sync::Lazy;
-use s3::presigning::PresigningConfig;
-use s3::Client;
-use salvo::http::header;
-use salvo::http::{HeaderValue, StatusCode};
-use salvo::prelude::*;
-use salvo::serve_static::StaticDir;
+use env_logger::Env;
+use s3::{presigning::PresigningConfig, Client};
 use serde::{Deserialize, Serialize};
+use std::env;
 use std::time::Duration;
-use std::{env, process};
-use tokio::sync::OnceCell;
-use tracing::error;
 use uuid::Uuid;
 use validator::Validate;
 
-static CORS_ALLOW_ORIGINS: Lazy<String> =
-    Lazy::new(|| env::var("CORS_ALLOW_ORIGINS").unwrap_or("*".to_string()));
-static CLIENT: OnceCell<Client> = OnceCell::const_new();
-static BUCKET_NAME: Lazy<String> = Lazy::new(|| {
-    let bucket_name = env::var("S3_BUCKET_NAME").unwrap_or("".to_string());
-
-    if bucket_name.is_empty() {
-        error!("Env 'S3_BUCKET_NAME' is empty.");
-        process::exit(1);
-    }
-
-    bucket_name
-});
+struct AppState {
+    s3_client: Client,
+}
 
 // Initialize S3 slient
-async fn init_client() -> Client {
-    let config = aws_config::load_defaults(BehaviorVersion::latest()).await;
+fn init_client(config: &SdkConfig) -> Client {
     let s3_endpoint = env::var("S3_ENDPOINT").unwrap_or("".to_string());
 
     if s3_endpoint.is_empty() {
         return aws_sdk_s3::Client::new(&config);
     }
 
-    let local_config = aws_sdk_s3::config::Builder::from(&config)
+    let local_config = aws_sdk_s3::config::Builder::from(config)
         .endpoint_url(s3_endpoint)
         .force_path_style(
             env::var("S3_FORCE_PATH_STYLE")
@@ -63,38 +48,23 @@ struct UploadResponse {
     download_url: String,
 }
 
-#[handler]
-async fn upload_url_handler(req: &mut Request, res: &mut Response) {
-    let _ = req.add_header(
-        header::CONTENT_TYPE,
-        HeaderValue::from_static("application/json"),
-        true,
-    );
+#[post("/upload-url")]
+async fn upload_url_handler(
+    req: web::Json<UploadRequest>,
+    data: web::Data<AppState>,
+) -> impl Responder {
+    let upload_request = req.into_inner();
 
-    let upload_request = req
-        .parse_json::<UploadRequest>()
-        .await
-        .map_err(|_| {
-            res.status_code(StatusCode::BAD_REQUEST)
-                .render(Text::Plain("Invalid UploadRequest json."))
-        })
-        .unwrap();
-
-    match upload_request.validate() {
-        Ok(_) => {}
-        Err(errors) => {
-            return res
-                .status_code(StatusCode::BAD_REQUEST)
-                .render(Text::Plain(errors.to_string()));
-        }
-    };
+    if let Err(errors) = upload_request.validate() {
+        return HttpResponse::BadRequest().body(errors.to_string());
+    }
 
     let file_name = format!("{}/{}", Uuid::new_v4(), upload_request.file_name);
     let expires_in_secs = upload_request.expires_in_secs.unwrap_or(86400 * 7);
-    let bucket_name = BUCKET_NAME.to_string();
-    let my_client = CLIENT.get().unwrap();
+    let bucket_name = env::var("S3_BUCKET_NAME").expect("Env 'S3_BUCKET_NAME' is empty.");
 
-    let result_upload = my_client
+    let result_upload = data
+        .s3_client
         .put_object()
         .bucket(&bucket_name)
         .key(file_name.clone())
@@ -105,65 +75,62 @@ async fn upload_url_handler(req: &mut Request, res: &mut Response) {
         )
         .await;
 
-    let result_download = my_client
+    let result_download = data
+        .s3_client
         .get_object()
         .bucket(&bucket_name)
-        .key(file_name.clone())
+        .key(file_name)
         .presigned(PresigningConfig::expires_in(Duration::from_secs(expires_in_secs)).unwrap())
         .await;
 
-    return res.render(Json({
-        UploadResponse {
-            upload_url: result_upload.unwrap().uri().to_string(),
-            download_url: result_download.unwrap().uri().to_string(),
-        }
-    }));
+    HttpResponse::Ok().json(UploadResponse {
+        upload_url: result_upload.unwrap().uri().to_string(),
+        download_url: result_download.unwrap().uri().to_string(),
+    })
 }
 
-#[handler]
-async fn content_type(_req: &mut Request, res: &mut Response) {
-    res.headers_mut().insert(
-        header::CONTENT_TYPE,
-        HeaderValue::from_static("application/json"),
-    );
+#[get("/status")]
+async fn ok() -> impl Responder {
+    HttpResponse::Ok().body("Ok")
 }
 
-#[handler]
-async fn cors(_req: &mut Request, res: &mut Response) {
-    res.headers_mut().insert(
-        header::ACCESS_CONTROL_ALLOW_ORIGIN,
-        HeaderValue::from_static(CORS_ALLOW_ORIGINS.as_str()),
-    );
-}
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let config = aws_config::load_defaults(BehaviorVersion::latest()).await;
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
 
-#[handler]
-async fn ok(res: &mut Response) {
-    res.status_code(StatusCode::OK).render(Text::Plain("OK"))
-}
+    HttpServer::new(move || {
+        let allowed_origins = env::var("CORS_ALLOWED_ORIGINS").unwrap_or("*".to_string());
+        let cors = Cors::default()
+            .allowed_methods(vec!["OPTIONS", "GET", "POST"])
+            .allow_any_header()
+            .allowed_origin_fn(move |origin, _| {
+                if allowed_origins == "*" {
+                    return true;
+                }
+                let allowed_origins_list: Vec<&str> = allowed_origins
+                    .split_terminator(",")
+                    .filter(|s| !s.is_empty())
+                    .collect();
+                allowed_origins_list.contains(&origin.to_str().unwrap())
+            });
 
-#[tokio::main]
-async fn main() {
-    tracing_subscriber::fmt().init();
-    CLIENT.get_or_init(init_client).await;
-
-    let router = Router::new()
-        .hoop(content_type)
-        .hoop(cors)
-        .push(Router::with_path("status").get(ok))
-        .push(
-            Router::with_path("upload-url")
-                .post(upload_url_handler)
-                .options(ok),
-        )
-        .push(Router::with_path("icon.svg").get(StaticFile::new("static/icon.svg")))
-        .push(
-            Router::with_path("<**path>").get(
-                StaticDir::new(["static"])
-                    .defaults("index.html")
-                    .auto_list(true),
-            ),
-        );
-
-    let acceptor = TcpListener::new("0.0.0.0:3000").bind().await;
-    Server::new(acceptor).serve(router).await;
+        App::new()
+            .app_data(web::Data::new(AppState {
+                s3_client: init_client(&config),
+            }))
+            .wrap(Logger::default())
+            .wrap(cors)
+            .service(upload_url_handler)
+            .service(ok)
+            .service(fs::Files::new("/icon.svg", "static").index_file("icon.svg"))
+            .service(
+                fs::Files::new("/", "static")
+                    .index_file("index.html")
+                    .show_files_listing(),
+            )
+    })
+    .bind("0.0.0.0:3000")?
+    .run()
+    .await
 }
