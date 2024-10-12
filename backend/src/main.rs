c21008503b048637788bc57d4923bf2d30a395f8
Jannick Viets <jannick@viets.email>
2024-10-12 16:24:15 +0200

    UI fixed

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,12 @@
 use actix_cors::Cors;
 use actix_files as fs;
 use actix_web::{get, middleware::Logger, post, web, App, HttpResponse, HttpServer, Responder};
-use aws_config::{BehaviorVersion, SdkConfig};
+use aws_config::SdkConfig;
 use aws_sdk_s3 as s3;
 use env_logger::Env;
 use s3::{presigning::PresigningConfig, Client};
 use serde::{Deserialize, Serialize};
 use std::env;
-use std::io::Error;
 use std::time::Duration;
 use uuid::Uuid;
 use validator::Validate;
@@ -16,24 +15,16 @@ struct AppState {
     s3_client: Client,
 }
 
-// Initialize S3 client
 fn init_client(config: &SdkConfig) -> Client {
-    let s3_endpoint = env::var("S3_ENDPOINT").unwrap_or("".to_string());
-
+    let s3_endpoint = env::var("S3_ENDPOINT").unwrap_or_default();
     if s3_endpoint.is_empty() {
-        return aws_sdk_s3::Client::new(&config);
+        return Client::new(config);
     }
-
     let local_config = aws_sdk_s3::config::Builder::from(config)
         .endpoint_url(s3_endpoint)
-        .force_path_style(
-            env::var("S3_FORCE_PATH_STYLE")
-                .unwrap_or("".to_string())
-                .eq("true"),
-        )
+        .force_path_style(env::var("S3_FORCE_PATH_STYLE").unwrap_or_default() == "true")
         .build();
-
-    return aws_sdk_s3::Client::from_conf(local_config);
+    Client::from_conf(local_config)
 }
 
 #[derive(Deserialize, Debug, Validate)]
@@ -67,87 +58,61 @@ struct LinkShortenerRequest {
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct LinkWithSlugUrl {
-    pub url_slug: Option<String>,
+struct LinkWithSlugUrl {
+    url_slug: Option<String>,
 }
 
-async fn generate_short_url(download_url: String, file_name: String) -> Result<String, Error> {
-    let link_shortener_endpoint = env::var("LINK_SHORTENER_ENDPOINT").unwrap_or("".to_string());
-    let link_shortener_random_slug = env::var("LINK_SHORTENER_RANDOM_SLUG")
-        .unwrap_or("".to_string())
-        .to_lowercase()
-        .eq("true");
-
+async fn generate_short_url(download_url: String, file_name: String) -> Result<String, Box<dyn std::error::Error>> {
+    let link_shortener_endpoint = env::var("LINK_SHORTENER_ENDPOINT").unwrap_or_default();
     if link_shortener_endpoint.is_empty() {
-        return Ok(download_url.clone())
+        return Ok(download_url);
     }
-
-    let slug = if link_shortener_random_slug {
+    let slug = if env::var("LINK_SHORTENER_RANDOM_SLUG").unwrap_or_default().eq_ignore_ascii_case("true") {
         "".to_string()
     } else {
-        file_name.clone()
+        file_name
     };
-
     let response = reqwest::Client::new()
         .post(&link_shortener_endpoint)
-        .json(&LinkShortenerRequest {
-            slug,
-            url: download_url.clone(),
-        })
+        .json(&LinkShortenerRequest { slug, url: download_url.clone() })
         .send()
-        .await;
-
-    let link: LinkWithSlugUrl = response.unwrap().json().await.unwrap();
-
-    Ok(link.url_slug.unwrap())
+        .await?;
+    let link: LinkWithSlugUrl = response.json().await?;
+    Ok(link.url_slug.unwrap_or(download_url))
 }
 
 #[post("/upload-url")]
-async fn upload_url_handler(
-    req: web::Json<UploadRequest>,
-    data: web::Data<AppState>,
-) -> impl Responder {
+async fn upload_url_handler(req: web::Json<UploadRequest>, data: web::Data<AppState>) -> Result<HttpResponse, Box<dyn std::error::Error>> {
     let upload_request = req.into_inner();
-
     if let Err(errors) = upload_request.validate() {
-        return HttpResponse::BadRequest().body(errors.to_string());
+        return Ok(HttpResponse::BadRequest().body(errors.to_string()));
     }
-
     let file_name = format!("{}/{}", Uuid::new_v4(), upload_request.file_name);
     let expires_in_secs = upload_request.expires_in_secs.unwrap_or(86400 * 7);
     let bucket_name = env::var("S3_BUCKET_NAME").expect("Env 'S3_BUCKET_NAME' is empty.");
-
-    let result_upload = data
-        .s3_client
+    let result_upload = data.s3_client
         .put_object()
         .bucket(&bucket_name)
-        .key(file_name.clone())
-        .presigned(
-            PresigningConfig::expires_in(Duration::from_secs(600))
-                .unwrap()
-                .clone(),
-        )
-        .await;
-
-    let mut download_url = data
-        .s3_client
+        .key(&file_name)
+        .presigned(PresigningConfig::expires_in(Duration::from_secs(600))?)
+        .await?;
+    let mut download_url = data.s3_client
         .get_object()
         .bucket(&bucket_name)
-        .key(file_name.clone())
-        .presigned(PresigningConfig::expires_in(Duration::from_secs(expires_in_secs)).unwrap())
-        .await.unwrap().uri().to_string();
-
+        .key(&file_name)
+        .presigned(PresigningConfig::expires_in(Duration::from_secs(expires_in_secs))?)
+        .await?
+        .uri()
+        .to_string();
     if env::var("LINK_SHORTENER_ENDPOINT").is_ok() {
-        match generate_short_url(download_url.clone(), file_name).await {
-            Ok(val) => download_url = val,
-            Err(..) => ()
+        if let Ok(val) = generate_short_url(download_url.clone(), file_name).await {
+            download_url = val;
         }
     }
-
-    HttpResponse::Ok().json(UploadResponse {
-        upload_url: result_upload.unwrap().uri().to_string(),
+    Ok(HttpResponse::Ok().json(UploadResponse {
+        upload_url: result_upload.uri().to_string(),
         download_url,
-    })
+    }))
 }
 
 #[get("/status")]
@@ -157,9 +122,8 @@ async fn ok() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let config = aws_config::load_defaults(BehaviorVersion::latest()).await;
+    let config = aws_config::load_from_env().await;
     env_logger::init_from_env(Env::default().default_filter_or("info"));
-
     HttpServer::new(move || {
         let allowed_origins = env::var("CORS_ALLOWED_ORIGINS").unwrap_or("*".to_string());
         let cors = Cors::default()
@@ -167,29 +131,19 @@ async fn main() -> std::io::Result<()> {
             .allow_any_header()
             .allowed_origin_fn(move |origin, _| {
                 if allowed_origins == "*" {
-                    return true;
+                    true
+                } else {
+                    allowed_origins.split(',').any(|s| s == origin.to_str().unwrap())
                 }
-                let allowed_origins_list: Vec<&str> = allowed_origins
-                    .split_terminator(",")
-                    .filter(|s| !s.is_empty())
-                    .collect();
-                allowed_origins_list.contains(&origin.to_str().unwrap())
             });
-
         App::new()
-            .app_data(web::Data::new(AppState {
-                s3_client: init_client(&config),
-            }))
+            .app_data(web::Data::new(AppState { s3_client: init_client(&config) }))
             .wrap(Logger::default())
             .wrap(cors)
             .service(ok)
             .service(upload_url_handler)
             .service(fs::Files::new("/icon.svg", "static").index_file("icon.svg"))
-            .service(
-                fs::Files::new("/", "static")
-                    .index_file("index.html")
-                    .show_files_listing(),
-            )
+            .service(fs::Files::new("/", "static").index_file("index.html").show_files_listing())
     })
         .bind("0.0.0.0:3000")?
         .run()
--- ui/src/Requests/api.ts
+++ ui/src/Requests/api.ts
@@ -2,36 +2,17 @@ const hostname =
   location.hostname === "localhost" ? "http://localhost:3000" : "";
 
 export interface CreateLinkRequest {
-  file_name: string;
-  expires_in_secs: number;
-}
-
-export interface UploadReadyRequest {
   fileName: string;
-  downloadUrl: string;
+  expiresInSecs: number;
 }
 
 export interface CreateLinkResponse {
-  upload_url: string;
-  download_url: string;
+  uploadUrl: string;
+  downloadUrl: string;
 }
 
 export async function fetchPresignedUrls(props: CreateLinkRequest) {
   const res = await fetch(`${hostname}/upload-url`, {
-    method: "POST",
-    body: JSON.stringify(props),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  if (!res.ok) {
-    throw new Error(await res.json());
-  }
-  return res.json() as Promise<CreateLinkResponse>;
-}
-
-export async function fetchUploadReady(props: UploadReadyRequest) {
-  const res = await fetch(`${hostname}/upload-ready`, {
     method: "POST",
     body: JSON.stringify(props),
     headers: {
